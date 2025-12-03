96db09fc57f8110408520b735a8fe3c311629a65
Juan-Velo <juan.velo@utec.edu.pe>
2025-12-03 00:46:34 -0500

    Consolidate implementado

--- fibonacci_heap.cpp
+++ fibonacci_heap.cpp
@@ -53,6 +53,56 @@ class FibonacciHeap {
             y->mark = false;
         }
 
+        void consolidate() {
+            if (minNode == nullptr) return;
+    
+            int max_degree = (int)(log(n) / log(1.618)) + 5;
+            vector<Node*> A(max_degree, nullptr);
+    
+            vector<Node*> rootList;
+            Node* x = minNode;
+            if (x) {
+                do {
+                    rootList.push_back(x);
+                    x = x->right;
+                } while (x != minNode);
+            }
+    
+            for (Node* w : rootList) {
+                Node* x = w;
+                int d = x->degree;
+                while (A[d] != nullptr) {
+                    Node* y = A[d];
+                    if (x->key > y->key) {
+                        swap(x, y);
+                    }
+                    link(y, x);
+                    A[d] = nullptr;
+                    d++;
+                }
+                A[d] = x;
+            }
+    
+            minNode = nullptr;
+            for (int i = 0; i < max_degree; i++) {
+                if (A[i] != nullptr) {
+                    if (minNode == nullptr) {
+                        minNode = A[i];
+                        minNode->left = minNode;
+                        minNode->right = minNode;
+                    } else {
+                        A[i]->left = minNode;
+                        A[i]->right = minNode->right;
+                        minNode->right->left = A[i];
+                        minNode->right = A[i];
+                        if (A[i]->key < minNode->key) {
+                            minNode = A[i];
+                        }
+                    }
+                }
+            }
+        }
+
     public:
         FibonacciHeap() {
             minNode = nullptr;
