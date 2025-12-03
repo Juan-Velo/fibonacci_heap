9a1b7f49d72b8c9d4bfec7a1f0a3a89e4b0d1d9f
ChRi505 <angrycri@gmail.com>
2025-12-03 00:38:32 -0500

    Commit 2

--- fibonacci_heap.cpp
+++ fibonacci_heap.cpp
@@ -8,8 +8,8 @@
 #include <sstream>
 
 
-
 using namespace std;
+
 struct Node {
     int key;
     int degree;
@@ -28,6 +28,36 @@ struct Node {
         left = this;
         right = this;
     }
+
+class FibonacciHeap {
+    private:
+        Node* minNode;
+        int n;
+
+        void link(Node* y, Node* x) {
+            y->left->right = y->right;
+            y->right->left = y->left;
+
+            y->p = x;
+            if (x->child == nullptr) {
+                x->child = y;
+                y->right = y;
+                y->left = y;
+            } else {
+                y->left = x->child;
+                y->right = x->child->right;
+                x->child->right->left = y;
+                x->child->right = y;
+            }
+            x->degree++;
+            y->mark = false;
+        }
+
+    public:
+        FibonacciHeap() {
+            minNode = nullptr;
+            n = 0;
+        }
 };
 
 
